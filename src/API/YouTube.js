ae2e712b8a1abda993166885d291a9d4d81f02f4
Ryan Carr <carr.ryanedward@gmail.com>
2021-03-03 12:13:35 -0500

    Added env files and updated gitignore

--- src/API/YouTube.js
+++ src/API/YouTube.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyALqFXRLesKZaCmDxjri2DrhYEB9PVi9Fc';
+const BASE_URL = process.env.REACT_APP_YOUTUBE_BASEURL;
+const KEY = process.env.REACT_APP_YOUTUBE_APIKEY;
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3',
+  baseURL: BASE_URL,
   params: {
     part: 'snippet',
     maxResults: 5,
